b78eb2bf60dac1791627a3ce47c6296528d468a4
testernew1 <testerprof@gmail.com>
2014-06-14 00:39:01 -0400

    L-03, HW-05e

--- addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -6,27 +6,27 @@ import static org.testng.Assert.assertEquals;
 import org.testng.annotations.Test;
 
 public class GroupCreationTests extends TestBase {
-	
-  @Test(dataProvider = "randomValidGroupGenerator")
-  public void testGroupCreationWithValidData(GroupData group) throws Exception {
-	app.getNavigationHelper().openMainPage();
-    app.getNavigationHelper().gotoGroupsPage();
 
-    // save old state
-    List<GroupData> oldList = app.getGroupHelper().getGroups();
+	@Test(dataProvider = "randomValidGroupGenerator")
+	public void testGroupCreationWithValidData(GroupData group) throws Exception {
+		app.getNavigationHelper().openMainPage();
+		app.getNavigationHelper().gotoGroupsPage();
 
-    // actions    
-    app.getGroupHelper().initGroupCreation();
-	app.getGroupHelper().fillGroupForm(group);
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupsPage();
+		// save old state
+		List<GroupData> oldList = app.getGroupHelper().getGroups();
 
-    // save new state
-    List<GroupData> newList = app.getGroupHelper().getGroups();
+		// actions    
+		app.getGroupHelper().initGroupCreation();
+		app.getGroupHelper().fillGroupForm(group);
+		app.getGroupHelper().submitGroupCreation();
+		app.getGroupHelper().returnToGroupsPage();
 
-    // compare states
-    oldList.add(group);
-    Collections.sort(oldList);
-    assertEquals(newList, oldList);
+		// save new state
+		List<GroupData> newList = app.getGroupHelper().getGroups();
+
+		// compare states
+		oldList.add(group);
+		Collections.sort(oldList);
+		assertEquals(newList, oldList);
   }
 }
--- addressbook-selenium-tests/src/com/example/tests/GroupRemovalTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupRemovalTests.java
@@ -7,7 +7,7 @@ import static org.testng.Assert.assertEquals;
 import org.testng.annotations.Test;
 
 public class GroupRemovalTests extends TestBase {
-	
+
 	@Test
 	public void deleteSomeGroup() {
 		app.getNavigationHelper().openMainPage();
